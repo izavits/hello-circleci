0419066cdcd6e553c50c1cafe7ff2b05b758bb47
izavits <izavits@gmail.com>
2018-03-06 19:57:54 +0200

    Test feature

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var HOST='0.0.0.0';
 var app=express();
 app.get('/', function(req, res) {
 	console.log('Received request!');
-	res.send('Hello!!');
+	res.send('Hello test feature!!');
 });
 app.listen(PORT, HOST);
 console.log('Running on http://'+HOST+':'+PORT);
